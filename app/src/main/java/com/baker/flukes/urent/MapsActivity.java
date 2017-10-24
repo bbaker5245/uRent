71415e91fabff9d6d4569cc889809ed4a71ef6bd
rflukes <flukes.2@osu.edu>
2017-10-23 23:28:14 -0400

    created method for converting address to latlng coordinates and add as maps marker

--- app/src/main/java/com/baker/flukes/urent/MapsActivity.java
+++ app/src/main/java/com/baker/flukes/urent/MapsActivity.java
@@ -1,7 +1,10 @@
 package com.baker.flukes.urent;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +13,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private static final String TAG = "MapsActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +48,40 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng osu = new LatLng(39.9976772, -83.0085753);
         mMap.setMinZoomPreference(15.0f);
         mMap.addMarker(new MarkerOptions().position(osu).title("OSU"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(osu));
+        String address = "132 East 12th Ave, Columbus, OH";
+        LatLng property = new LatLng(39.9976762, -83.0085753);
+        try {
+            property = getLocationFromAddress(address);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            Log.d(TAG,getLocationFromAddress(address).toString());
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mMap.addMarker(new MarkerOptions().position(property).title(address));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(property));
+    }
+
+    public  LatLng getLocationFromAddress(String strAddress) throws IOException {
+
+        Geocoder coder = new Geocoder(getApplicationContext());
+        List<Address> address;
+        LatLng p1 = null;
+
+        try {
+            address = coder.getFromLocationName(strAddress,5);
+            if (address==null) {
+                return null;
+            }
+            Address location=address.get(0);
+            p1 = new LatLng(location.getLatitude(), location.getLongitude());
+            return p1;
+        }catch (Exception e){
+            System.out.print(e);
+        }
+        return p1;
     }
 }
