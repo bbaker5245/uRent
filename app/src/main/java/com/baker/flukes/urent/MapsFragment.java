630069aad2081145d3f556c7fdcffec6b89c09ed
rflukes <flukes.2@osu.edu>
2017-10-30 21:40:53 -0400

    added use of location services with google maps

---
+++ app/src/main/java/com/baker/flukes/urent/MapsFragment.java
@@ -0,0 +1,238 @@
+package com.baker.flukes.urent;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.Manifest;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by rflukes on 10/30/17.
+ */
+
+public class MapsFragment extends SupportMapFragment {
+    private GoogleMap mMap;
+    private static final String TAG = "MapsFragment";
+    private static final int REQUEST_LOCATION_PERMISSIONS = 0;
+    private static final int REQUEST_ERROR = 0;
+    private static final String[] LOCATION_PERMISSIONS = new String[]{
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+    };
+    private GoogleApiClient mClient;
+    private Location mLocation;
+    private boolean mLocationPermissionGranted = false;
+
+    public static MapsFragment newInstance() {
+        return new MapsFragment();
+    }
+
+    private void getLocation() {
+
+        LocationRequest request = LocationRequest.create();
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setNumUpdates(1);
+        request.setInterval(0);
+        Location mLocation = LocationServices.FusedLocationApi.getLastLocation(mClient);
+        if(mLocation != null){
+            Log.d(TAG, "Last Location found");
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(new LatLng(mLocation.getLatitude(), mLocation.getLongitude()));
+            markerOptions.title("Current Position");
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+            mMap.addMarker(markerOptions);
+        }else{
+            Log.d(TAG, "Last Location was Null");
+        }
+        LocationServices.FusedLocationApi
+                .requestLocationUpdates(mClient, request, new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        Log.i(TAG, "Got a fix: " + location);
+                    }
+                });
+    }
+
+    private boolean hasLocationPermission() {
+        int result = ContextCompat
+                .checkSelfPermission(getActivity(), LOCATION_PERMISSIONS[0]);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
+        mClient = new GoogleApiClient.Builder(getActivity())
+                .addApi(LocationServices.API).addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(Bundle bundle) {
+                        getActivity().invalidateOptionsMenu();
+                        getLocation();
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int i) {
+
+                    }
+                }).build();
+
+        getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mMap = googleMap;
+                LatLng osu = new LatLng(39.9976772, -83.0085753);
+                mMap.setMinZoomPreference(15.0f);
+                mMap.addMarker(new MarkerOptions().position(osu).title("OSU"));
+                String address = "132 East 12th Ave, Columbus, OH";
+                LatLng property = new LatLng(39.9976762, -83.0085753);
+                try {
+                    property = getLocationFromAddress(address);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    Log.d(TAG, getLocationFromAddress(address).toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                //mMap.addMarker(new MarkerOptions().position(property).title(address));
+
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(LOCATION_PERMISSIONS,
+                            REQUEST_LOCATION_PERMISSIONS);
+                }
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(osu));
+            }
+        });
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mClient.connect();
+        getActivity().invalidateOptionsMenu();
+
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int errorCode = apiAvailability.isGooglePlayServicesAvailable(getContext());
+
+        if (errorCode != ConnectionResult.SUCCESS) {
+            Dialog errorDialog = apiAvailability
+                    .getErrorDialog(getActivity(), errorCode, REQUEST_ERROR,
+                            new DialogInterface.OnCancelListener() {
+
+                                @Override
+                                public void onCancel(DialogInterface dialog) {
+                                    // Leave if services are unavailable.
+                                    getActivity().finish();
+                                }
+                            });
+
+            errorDialog.show();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        mClient.disconnect();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_maps, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_locate);
+        searchItem.setEnabled(mClient.isConnected());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_locate:
+                if (hasLocationPermission()) {
+                    Log.d(TAG, "hasLocationPermission");
+                    getLocation();
+                }else {
+                    requestPermissions(LOCATION_PERMISSIONS,
+                            REQUEST_LOCATION_PERMISSIONS);
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSIONS:
+                if (hasLocationPermission()) {
+                    getLocation();
+                }
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+    public  LatLng getLocationFromAddress(String strAddress) throws IOException {
+
+        Geocoder coder = new Geocoder(getContext());
+        List<Address> address;
+        LatLng p1 = null;
+
+        try {
+            address = coder.getFromLocationName(strAddress,5);
+            if (address==null) {
+                return null;
+            }
+            Address location=address.get(0);
+            p1 = new LatLng(location.getLatitude(), location.getLongitude());
+            return p1;
+        }catch (Exception e){
+            System.out.print(e);
+        }
+        return p1;
+    }
+}
